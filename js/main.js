7e8f2ec4711dc32ae7c16383270460029711d8cd
10-ui <ui-10@outlook.jp>
2023-07-11 13:27:02 +0900

    none

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 'use strict'
 
 
-const hbgBtn = document.querySelector('.btn');
-const lines = document.querySelector('.lines');
-const onState = document.querySelector('.onState');
-const offState = document.querySelector('.offState');
+const hbgBtn = document.querySelector('.menuBtn');
+const lines = document.querySelector('.lineChar');
 const activeMenu = document.querySelector('.menu-list')
 
 
 hbgBtn.addEventListener('click',()=>{
   lines.classList.toggle('active-hbg-line');
-  onState.classList.toggle('onState-active');
-  offState.classList.toggle('offState-active');
   activeMenu.classList.toggle('menu-list-active');
 });
