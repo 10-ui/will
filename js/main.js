4df32ff509b0e746cb6541f86f9c8d8f3a3c97ed
10-ui <ui-10@outlook.jp>
2023-07-12 15:39:28 +0900

    変更&追加

--- js/main.js
+++ js/main.js
@@ -4,20 +4,28 @@
 const hbgBtn = document.querySelector('.menuBtn');
 const lines = document.querySelector('.menuBtn div');
 const body = document.querySelector('.menu-body');
+const html = document.querySelector('html');
+const header = document.querySelector('header');
 
 hbgBtn.addEventListener('click',()=>{
   lines.classList.toggle('active');
-  body.classList.toggle('menu-body-ac')
+  body.classList.toggle('menu-body-ac');
   if(lines.innerHTML === 'Menu'){
     lines.innerHTML = 'Close'
+    html.style.overflow = 'hidden'
   }
-  else lines.innerHTML = 'Menu';
+  else {lines.innerHTML = 'Menu'
+        html.style.overflow = 'visible'}
 });
 
 const swiper = new Swiper('.swiper',{
   loop: true,
   speed: 3000,
+  allowTouchMove: false,
+  loopedSlides: 2,
+  slidesPerView: "auto",
   autoplay: {
     delay: 0,
+    disableOnInteraction: false,
   },
 });
