3f1b542eae8f65f36fa709916601d1eaa27b11c9
Sentaro <ui-10@outlook.jp>
2023-07-22 19:49:26 +0900

    gnav情報を修正

--- js/webweek.js
+++ js/webweek.js
@@ -1,17 +1,35 @@
 'use strict'
 
-const back = document.querySelector('.wrapper');
+const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) );
+const Menu = document.querySelector('.menu');
+const curchar = document.querySelector('.current');
+const list = document.querySelector('.weekList');
+const current = document.querySelector('body').getAttribute('data-current')
 
+list.style.display = 'none'
+let state = 0;
 
-// gsap.to(back,{
-//   scrollTrigger: {
-//     trigger: back,
-//     start: 'top top',
-//     end: () => `+=${back.clientWidth}`,
-//     scrub: true,
-//     pin: true,
-//     antipatePin: 1,
-//     invalidateOnRefresh: true,
-//     markers: true,
-//   },
-// })
+Menu.addEventListener('click',()=>{
+  if (state===0){
+    open();
+    state = 1
+  }
+  else {
+    close();
+    state = 0
+  }
+});
+
+async function open() {
+  curchar.innerHTML = null;
+  Menu.classList.add('open');
+  await sleep(1000);
+  list.style.display = 'block';
+}
+
+async function close() {
+  Menu.classList.remove('open');
+  list.style.display = 'none'
+  await sleep(1000);
+  curchar.innerHTML = current;
+}
