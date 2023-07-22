d29f4263429513c0ffa8ad18fd2f392f3d8f29d9
Sentaro <ui-10@outlook.jp>
2023-07-22 14:58:22 +0900

    第一段階Clear

---
+++ js/webweek.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const back = document.querySelector('.wrapper');
+
+
+// gsap.to(back,{
+//   scrollTrigger: {
+//     trigger: back,
+//     start: 'top top',
+//     end: () => `+=${back.clientWidth}`,
+//     scrub: true,
+//     pin: true,
+//     antipatePin: 1,
+//     invalidateOnRefresh: true,
+//     markers: true,
+//   },
+// })
