619bb62de3888237c7a47643c772daf9a14b2722
Sentaro <ui-10@outlook.jp>
2023-07-24 07:02:36 +0900

    ss

--- js/webweek.js
+++ js/webweek.js
@@ -4,19 +4,22 @@ const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) 
 const Menu = document.querySelector('.menu');
 const curchar = document.querySelector('.current');
 const list = document.querySelector('.weekList');
-const current = document.querySelector('body').getAttribute('data-current')
+const current = document.querySelector('body').getAttribute('data-current');
+const dayindex = document.querySelectorAll('.weekList li');
+const Mon = document.querySelector('.mon');
+const clas = document.querySelector('.class');
 
 list.style.display = 'none'
 let state = 0;
 
 Menu.addEventListener('click',()=>{
   if (state===0){
-    open();
     state = 1
+    open();
   }
   else {
-    close();
     state = 0
+    close();
   }
 });
 
@@ -28,8 +31,14 @@ async function open() {
 }
 
 async function close() {
+  curchar.innerHTML = current;
   Menu.classList.remove('open');
   list.style.display = 'none'
   await sleep(1000);
-  curchar.innerHTML = current;
 }
+
+// gsap.to(Mon, {
+//   opacity: 0,
+//   y: 100,
+//   duration: 2
+// });
